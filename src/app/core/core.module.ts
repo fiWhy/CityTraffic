b7ee9c3b494a995234de8c1219f1ac76731e8cec
Denis <fiwhy@mail.ru>
2017-05-18 15:24:00 +0300

    Resolved compile warnings

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,12 @@
 import * as angular from "angular";
 
-import { CacheService } from "./services/cache.service";
-import { NetworkService } from "./services/network.service";
-import { AuthService } from "./services/auth/auth.service";
-import { GeoService } from "./services/geo.service";
-import { ToastService } from "./services/toast.service";
-
 import { ProvidersModule } from "./providers/providers.module";
+import { ServicesModule } from "./services/services.module";
 
 import { coreConstants } from "./core.constants";
 
 export const CoreModule: ng.IModule = angular.module("app.core", [
     ProvidersModule.name,
-]).service(CacheService.name, CacheService)
-    .service(NetworkService.name, NetworkService)
-    .service(AuthService.name, AuthService)
-    .service(GeoService.name, GeoService)
-    .service(ToastService.name, ToastService)
+    ServicesModule.name,
+])
     .constant("CoreConstants", coreConstants());
---
+++ src/app/core/services/services.module.ts
@@ -0,0 +1,13 @@
+import { CacheService } from "./cache.service";
+import { NetworkService } from "./network.service";
+import { AuthService } from "./auth/auth.service";
+import { GeoService } from "./geo.service";
+import { ToastService } from "./toast.service";
+
+export const ServicesModule: ng.IModule = angular.module("app.core.services", [
+])
+    .service(CacheService.name, CacheService)
+    .service(NetworkService.name, NetworkService)
+    .service(AuthService.name, AuthService)
+    .service(GeoService.name, GeoService)
+    .service(ToastService.name, ToastService)
